c2dac322a41d46e9e8b76efe1276faf53886b87d
Gyanenjay Rout <88276612+gyanenjayrout@users.noreply.github.com>
2022-08-09 10:28:37 +0530

    Add files via upload

---
+++ recurssion.java
@@ -0,0 +1,14 @@
+public class recurssion{
+    public static int fact(int n)
+    {
+        if(n==0||n==1)
+        {
+            return 1;
+        }
+        return fact(n-1)*n;
+    }
+    public static void main(String args[])
+    {
+        System.out.println(fact(5));
+    }
+}
